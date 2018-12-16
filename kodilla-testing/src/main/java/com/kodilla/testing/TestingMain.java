156357896a6f66d38ecee2e5c82468b7fbe4f46f
mpastusiak@live.com <mpastusiak@live.com>
2018-12-16 15:32:57 +0100

    6.2

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,39 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        Calculator calculator = new Calculator();
+
+        int addResult = calculator.add(2,3);
+        int equalsAddResult = 5;
+
+        if(addResult == equalsAddResult) {
+            System.out.println("ADD test OK - wynik " + equalsAddResult);
+        } else {
+            System.out.println("ADD test ERROR");
+        }
+
+        int subtractResult = calculator.subtract(9,3);
+        int equalsSubtractResult = 6;
+
+        if(subtractResult == equalsSubtractResult) {
+            System.out.println("SUBTRACT test OK - wynik " + equalsSubtractResult);
+        } else {
+            System.out.println("SUBTRACT test ERROR");
+        }
+
     }
 }
