8b7a1fcbbde5e95f246d618b31039a6c7dd62ebb
mpastusiak@live.com <mpastusiak@live.com>
2018-12-16 17:50:12 +0100

    6.3

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -5,35 +5,6 @@ import com.kodilla.testing.calculator.Calculator;
 
 public class TestingMain {
     public static void main(String[] args){
-        SimpleUser simpleUser = new SimpleUser("theForumUser");
-
-        String result = simpleUser.getUsername();
-
-        if (result.equals("theForumUser")){
-            System.out.println("test OK");
-        } else {
-            System.out.println("Error!");
-        }
-
-        Calculator calculator = new Calculator();
-
-        int addResult = calculator.add(2,3);
-        int equalsAddResult = 5;
-
-        if(addResult == equalsAddResult) {
-            System.out.println("ADD test OK - wynik " + equalsAddResult);
-        } else {
-            System.out.println("ADD test ERROR");
-        }
-
-        int subtractResult = calculator.subtract(9,3);
-        int equalsSubtractResult = 6;
-
-        if(subtractResult == equalsSubtractResult) {
-            System.out.println("SUBTRACT test OK - wynik " + equalsSubtractResult);
-        } else {
-            System.out.println("SUBTRACT test ERROR");
-        }
 
     }
 }
---
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -0,0 +1,67 @@
+package com.kodilla.testing.collection;
+import org.junit.*;
+
+import java.util.ArrayList;
+
+public class CollectionTestSuite {
+    @Before
+    public void before(){
+        System.out.println("Rozpoczęto test klasy wybierającej parzyste liczby z kolekcji");
+    }
+    @After
+    public void after(){
+        System.out.println("Zakończono test klasy wybierającej parzyste liczby z kolekcji");
+    }
+    @BeforeClass
+    public static void beforeClass() {
+        System.out.println("");
+    }
+    @AfterClass
+    public static void afterClass() {
+        System.out.println("");
+    }
+    @Test
+    public void testOddNumbersExterminatorEmptyList(){
+        //Given
+        ArrayList<Integer> checkArray = new ArrayList<Integer>();
+        ArrayList<Integer> checkOddArray = new ArrayList<Integer>();
+        //When
+        OddNumbersExterminator simpleArray = new OddNumbersExterminator();
+        ArrayList<Integer> resultArray = simpleArray.exterminate(checkArray);
+        System.out.println("Testing " + resultArray);
+        //Then
+        Assert.assertEquals(checkOddArray, resultArray);
+    }
+    @Test
+    public void testOddNumbersExterminatorNormalList(){
+        //Given
+        ArrayList<Integer> checkArray = new ArrayList<Integer>();
+        ArrayList<Integer> checkOddArray = new ArrayList<Integer>();
+
+        /* automatyczne dodawanie liczb do przykładowych tablic
+        for(int i = 1; i < 10; i++){
+            checkArray.add(i);
+            if(i % 2 == 0){
+                checkOddArray.add(i);
+            }
+        }
+        */
+        checkArray.add(1);
+        checkArray.add(2);
+        checkArray.add(3);
+        checkArray.add(4);
+        checkArray.add(5);
+        checkArray.add(6);
+
+        checkOddArray.add(2);
+        checkOddArray.add(4);
+        checkOddArray.add(6);
+        //When
+        OddNumbersExterminator simpleArray = new OddNumbersExterminator();
+        ArrayList<Integer> resultArray = simpleArray.exterminate(checkArray);
+        System.out.println("Testing " + resultArray);
+        //Then
+        Assert.assertEquals(checkOddArray, resultArray);
+    }
+
+}
