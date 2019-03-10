fb2db6c7583c4589973dd0f7af4aa610ad3e53ca
mpastusiak@live.com <mpastusiak@live.com>
2019-03-10 13:17:48 +0100

    15.5

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/builder/bigmac/BigmacTestSuite.java
@@ -0,0 +1,23 @@
+package com.kodilla.patterns.builder.bigmac;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BigmacTestSuite {
+    @Test
+    public void testBigmacNew() {
+        //Given
+        Bigmac bigmac = new Bigmac.BigmacBuilder()
+                .bun("sesame")
+                .burgers(2)
+                .sauce("light")
+                .ingredient("bacon")
+                .ingredient("chili")
+                .build();
+        System.out.println(bigmac);
+        //When
+        int howManyIngredients = bigmac.getIngredients().size();
+        //Then
+        Assert.assertEquals(2, howManyIngredients);
+    }
+}
